a6275c391da01f22eec468ab9a45aade91150f75
Alan Sánchez <alan247@gmail.com>
2018-01-20 21:44:27 -0600

    Add mapStateToProps

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker'
 import { createStore } from 'redux'
 import reducer from './reducers'
-import { Provider } from 'react-redux'
+import { Provider, connect } from 'react-redux'
+
 
 
 const store = createStore(
